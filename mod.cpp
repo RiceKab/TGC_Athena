a305091a1583500ffaab963e075988186f27d304
Kaboose <kevintjet@hotmail.com>
2016-10-25 18:11:03 +0200

    Removed version number from tooltip

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,5 @@
 name = "TGC Athena";
 picture = "tgclogo.paa";
+tooltip = "TGC Athena Beta";
 overview = "Small equipment and behaviour additions for editor and Zeus (Ares extension).";
 author = "RiceKab";
