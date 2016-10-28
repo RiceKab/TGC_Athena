bf185a1d1fa4f2348ec67ab19907b1abd4521bf7
Kaboose <kevintjet@hotmail.com>
2016-10-28 09:14:29 +0200

    Added Version Number to Tooltip (I changed my mind)

--- mod.cpp
+++ mod.cpp
@@ -1,5 +1,5 @@
 name = "TGC Athena";
 picture = "tgclogo.paa";
-tooltip = "TGC Athena Beta";
+tooltip = "TGC Athena Version 3";
 overview = "Small equipment and behaviour additions for editor and Zeus (Ares extension).";
 author = "RiceKab";
